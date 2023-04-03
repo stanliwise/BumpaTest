214adb92aa085c6d3a027a56b0cc4c5cd5d84f5f
stanliwise <31096369+stanliwise@users.noreply.github.com>
2023-04-03 11:40:45 +0100

    push additoinal update

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -13,7 +13,7 @@ class AchievementController extends Controller
     public function info(User $user)
     {
         $next_badge = $$user->current_badge()->count() == 0 ? Badge::first() : $user->current_badge()->first()->next_badge();
-        
+
         return response()->json([
             'unlocked_achievement' => $user->achievements()->pluck('name'),
             'next_available_achievement' => Achievement::where(
@@ -23,8 +23,7 @@ class AchievementController extends Controller
             )->pluck('name'),
             'current_badge' => $user->current_badge()->value('name'),
             'next_badge' => $user->current_badge()->count() == 0 ? Badge::pluck('name')->first() : $user->current_badge()->first()->next_badge()->value('name'),
-            'remaining_to_unlock_next_badge' => $user->current_badge()->value('achievement_count')
-
+            'remaining_to_unlock_next_badge' => ((int) $next_badge->achievement_count) - ((int) $user->achievements()->count())
         ], Response::HTTP_OK);
     }
 }
